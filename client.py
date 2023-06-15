dac71b55a19a95e452bbb68ff74027ca7751fc57
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-06-15 09:32:39 -0400

    update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 from scapy.all import *
+ip="127.0.0.1"
 while True:
 	i = input("Command: ")
 	cmd = "cmd " + i
-	packet = IP(dst="127.0.0.1")/ICMP()/cmd
+	packet = IP(dst=ip)/ICMP()/cmd
 	send(packet, verbose=0)
